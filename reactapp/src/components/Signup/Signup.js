0f657ffbaf1944f9854163de8930509ed089d84f
Harsh864 <sahuharsh0811@gmail.com>
2023-07-07 18:28:47 +0000

    new content

--- reactapp/src/components/Signup/Signup.js
+++ reactapp/src/components/Signup/Signup.js
@@ -1,104 +1,127 @@
-import React, { useState } from 'react'
-import './Signup.css'
-import { toast } from "react-toastify"
+import { useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Signup = () => {
-  const [credential, setCredential] = useState({ userName: "", name: "", email: "", password: "", dobDate: "" });
-  
-  const signup = async (e) => {
-    e.preventDefault();
-    try {
-      const { userName, name, email, password, dobDate } = credential;
-      if (userName.trim()=='' || name.trim()=='' || email.trim()=='' || password.trim()=='' || dobDate.trim()=='') {
-        toast.error("Please fill in all the required fields");
-        return;
-      }
-      const res = await fetch("https://8080-caafafebfadfbeacafdccaeaeaadbdbabf.project.examly.io/api/register", {
-        method: "POST",
-        credentials: 'include',
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ userName, name, email, password, dobDate })
-      })
-      const data = await res.text();
-      if (data !== "Email already exists") {
-        toast.success("Successfully Signed Up");
-        setCredential({ userName: "", name: "", email: "", password: "", dobDate: "" });
-      } else {
-        toast.error("Email already exists");
-      }
-      
+    const [credential, setCredential] = useState({ userName: "", name: "", email: "", password: "", dobDate: "" });
+
+    const signup = async (e) => {
+        e.preventDefault();
+        try {
+            const { userName, name, email, password, dobDate } = credential;
+            if (userName.trim() == '' || name.trim() == '' || email.trim() == '' || password.trim() == '' || dobDate.trim() == '') {
+                toast.error("Please fill in all the required fields");
+                return;
+            }
+            const res = await fetch("https://8080-caafafebfadfbeacafdccaeaeaadbdbabf.project.examly.io/api/register", {
+                method: "POST",
+                credentials: 'include',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ userName, name, email, password, dobDate })
+            })
+            const data = await res.text();
+            if (data !== "Email already exists") {
+                toast.success("Successfully Signed Up");
+                setCredential({ userName: "", name: "", email: "", password: "", dobDate: "" });
+            } else {
+                toast.error("Email already exists");
+            }
 
 
 
-    } catch (error) {
-      toast.error(error.message);
+
+        } catch (error) {
+            toast.error(error.message);
+        }
     }
-  }
 
-  const onChange = (e) => {
-    setCredential({ ...credential, [e.target.name]: e.target.value });
-  }
+    const onChange = (e) => {
+        setCredential({ ...credential, [e.target.name]: e.target.value });
+    }
 
-  return (
+    return (
+        <>
+            <form onSubmit={signup} className="sign-up-form">
+                <h2 className="title">Sign up</h2>
+                <div className="input-field">
+                    <i className="fas fa-user"></i>
+                    <input
+                        type="text"
+                        placeholder="Username"
+                        value={credential.userName}
+                        name="userName"
+                        onChange={onChange}
+                        required autoComplete="off"
+                    />
+                </div>
+                <div className="input-field">
+                    <i className="fas fa-user"></i>
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        name="name"
+                        value={credential.name}
+                        onChange={onChange}
+                        required autoComplete="off"
+                    />
+                </div>
+                <div className="input-field">
+                    <i className="fas fa-envelope"></i>
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        name="email"
+                        value={credential.email}
+                        onChange={onChange}
+                        required autoComplete="off"
+                        
+                    />
+                </div>
+                <div className="input-field">
+                    <i className="fas fa-lock"></i>
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        value={credential.password}
+                        onChange={onChange}
+                        required autoComplete="off"
+                    />
+                </div>
+                <div className="input-field">
+                    <i className="fas fa-calendar"></i>
+                    <input
+                        type="date"
+                        placeholder="Date Of Birth"
+                        name="dobDate"
+                        value={credential.dobDate}
+                        onChange={onChange}
+                        required autoComplete="off"
+                    />
+                </div>
 
-    <div className='bodyy'>
-      <section> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+                <input type="submit" className="btn" value="Sign up" />
+                {/* <p className="social-text">Or Sign up with social platforms</p> */}
+                {/* <div className="social-media">
+                    <button className="social-icon">
+                        <i className="fab fa-facebook-f"></i>
+                    </button>
+                    <button className="social-icon">
+                        <i className="fab fa-twitter"></i>
+                    </button>
+                    <button className="social-icon">
+                        <i className="fab fa-google"></i>
+                    </button>
+                    <button className="social-icon">
+                        <i className="fab fa-linkedin-in"></i>
+                    </button>
+                </div> */}
+            </form>
+            <ToastContainer />
+        </>
+    );
+};
 
-        <div className="signin">
-
-          <div className="content">
-
-            <h2>Sign Up</h2>
-
-            <div className="form">
-
-              <div className="inputBox">
-
-                <input type="text" value={credential.userName} className="box2" name='userName' onChange={onChange} required={true} /> <i>Username</i>
-
-              </div>
-
-              <div className="inputBox">
-
-                <input type="text" value={credential.name} className="box2" name='name' onChange={onChange} required /> <i>Name</i>
-
-              </div>
-
-              <div className="inputBox">
-
-                <input type="email" value={credential.email} className="box2" name='email' onChange={onChange} required /> <i>Email</i>
-
-              </div>
-
-              <div className="inputBox">
-
-                <input type="password" value={credential.password} className="box2" name='password' onChange={onChange} required /> <i>Password</i>
-
-              </div>
-
-              <div className="inputBox">
-
-                <input type="Date" value={credential.dobDate} placeholder="" className="box2" name='dobDate' onChange={onChange} required /> <i>Date Of Birth</i>
-
-              </div>
-
-              <div className="inputBox">
-
-                <input type="submit" onClick={signup} value="Sign Up" />
-
-              </div>
-
-            </div>
-
-          </div>
-
-        </div>
-
-      </section>
-    </div>
-  )
-}
-
-export default Signup
+export default Signup;
