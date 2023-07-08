d8755fa1c1ceab469225f5d51ef3c3ba97643846
ALOKARYAN51 <alokaryan51@gmail.com>
2023-07-08 12:10:47 +0000

    created folder structure

---
+++ reactapp/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Home from './Components/HomePage/HomePage';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
