ecb0af3e2fa34e43cbd67f3d47e7ebd707293567
vennela2 <saivennela.1403434@gmail.com>
2023-07-12 09:04:33 +0000

    Done.

---
+++ springapp/src/main/java/com/example/springapp/model/Comment.java
@@ -0,0 +1,19 @@
+package com.example.springapp.model;
+
+public class Comment {
+    private int postId;
+    private String text;
+    private String createdDate;
+
+    // Constructors
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    // Getters and setters for other fields
+}
