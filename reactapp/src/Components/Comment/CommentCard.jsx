f5771f2c6479449153801a6f736ceba05fc14882
AyushSoni-git <ayushsonirbt@gmail.com>
2023-07-28 06:13:57 +0000

    Updated code

--- reactapp/src/Components/Comment/CommentCard.jsx
+++ reactapp/src/Components/Comment/CommentCard.jsx
@@ -48,7 +48,7 @@ const CommentCard = ({ comment }) => {
           </div>
           <div className="ml-3">
             <p >
-              <span className="font-semibold"> Love Angle</span>
+              <span className="font-semibold"> {user?.username}</span>
                <span className="ml-2">{comment.content}</span>
             </p>
             <div className="flex items-center space-x-3 text-xs opacity-60 pt-2">
