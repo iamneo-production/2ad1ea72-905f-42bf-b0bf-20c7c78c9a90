d6530cd4ce2ab3d5a078aecc89fdac85bdfb7b26
Prashanth-01 <prashanthkumar2701@gmail.com>
2023-07-29 13:07:39 +0000

    Testcases.

--- springapp/src/main/java/com/example/springapp/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springapp.repository;
-
-import com.example.springapp.model.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommentRepository extends JpaRepository<Comment, Integer> {
-}
