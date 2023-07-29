097ad58e6f0e82b57bfa16fbbe18900b976f17da
vennela2 <saivennela.1403434@gmail.com>
2023-07-29 12:43:15 +0000

    Testcases

--- springapp/src/main/java/com/study/springapp/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.study.springapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.study.springapp.model.Comments;
-
-
-public interface CommentRepository extends JpaRepository<Comments, Integer> {
-
-}
